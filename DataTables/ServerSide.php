c6a49c47f9bcddfbdbd76db70836b15120473f87
Ulrich Völkel <mail@ulrich-voelkel.de>
2016-12-04 14:02:32 +0100

    revert 364acf08b57f0be66fc67a3934f9dcd1e3fc7b0d.

--- DataTables/ServerSide.php
+++ DataTables/ServerSide.php
@@ -79,6 +79,9 @@ class ServerSide
             }
         }
 
+        // order
+        $this->applyOrder($qb);
+
         // paginate
         $paginate = clone $qb;
         $paginate->select('distinct(' . $this->table->getPrefix() . '.' . $this->getIdentifierField() . ')');
@@ -97,9 +100,6 @@ class ServerSide
 
         $paginate->setFirstResult($this->request->getStart())->setMaxResults($this->request->getLength());
         $ids = $paginate->getQuery()->getResult();
-
-        // order
-        $this->applyOrder($qb);
 
         $qb->andWhere($this->table->getPrefix() . '.' . $this->getIdentifierField() . ' in (:ids)')
             ->setParameter('ids', $ids);
