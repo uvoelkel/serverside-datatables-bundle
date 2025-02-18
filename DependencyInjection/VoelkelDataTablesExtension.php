b680c32808c7157e2e53d315723f6427a3d6033c
Ulrich Völkel <ulrich.voelkel@baseplus.de>
2025-02-18 08:20:09 +0100

    fixed deprecation.

--- DependencyInjection/VoelkelDataTablesExtension.php
+++ DependencyInjection/VoelkelDataTablesExtension.php
@@ -2,7 +2,7 @@
 
 namespace Voelkel\DataTablesBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\FileLocator;
